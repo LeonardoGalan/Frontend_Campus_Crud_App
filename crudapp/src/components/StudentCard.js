7103bf63aeec1c2ea6e3b4a098a3d2991e3dab0f
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-01-20 13:55:31 -0500

    basic student card component. Issue #13

---
+++ crudapp/src/components/StudentCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import profile from "../images/vince-veras.jpg";
+import "../styles/StudentCard.css";
+
+export default function StudentCard(props) {
+  return (
+    <div className="student-card">
+      <img className="student-img" src={profile} alt="student-profile-pic" />
+      <h2 className="student-name">Student Full Name</h2>
+      <h3 className="student-campus-name">Campus Name</h3>
+      <p className="student-email">Email</p>
+      <p className="student-gpa">GPA</p>
+    </div>
+  );
+}
